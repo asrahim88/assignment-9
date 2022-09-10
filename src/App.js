5d9a1cb77fc84b917be20ea3c644332527e9ef76
asrahim88 <tasmimhasan1997@gmail.com>
2022-09-10 13:29:52 +0600

    add tailwind css and add react router dom and add flowbit plugin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Testing from './Components/Testing/Testing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-400">
+        Hello world!
+      </h1>
+      <Routes>
+        <Route path='/testing' element = {<Testing></Testing>} />
+      </Routes>
     </div>
   );
 }
